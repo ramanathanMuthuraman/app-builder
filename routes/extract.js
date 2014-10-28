0af833d0f3abe1d6c94e93df337de313e06d3100
ramanathanMuthuraman <ramanathan.gksvv@gmail.com>
2014-10-28 23:46:50 +0530

    intial message

--- routes/extract.js
+++ routes/extract.js
@@ -18,33 +18,32 @@ router.post('/', function(req, res) {
             token: '-aNx9JXc5brk2rdg1z7m'
         }, function(e, api) {
             if (e) throw e;
-              // time to make requests, on successful login
+            // time to make requests, on successful login
             getApp(api)
-              
+
         });
 
     };
 
-    function getApp(api){
+    function getApp(api) {
         api.get('/apps', function(e, data) {
             if (e) throw e;
             console.log('data:', data);
-            if(data.apps.length){
-            deleteApp(api,data.apps[0].id)
-        }
-      else{
+            if (data.apps.length) {
+                deleteApp(api, data.apps[0].id)
+            } else {
                 postApp(api)
-           }
+            }
         });
     }
 
 
-    function deleteApp(api,id){
-        api.del('/apps/'+id, function(e, data) {
-    console.log('error:', e);
-    console.log('data:', data);
-     postApp(api)
-});
+    function deleteApp(api, id) {
+        api.del('/apps/' + id, function(e, data) {
+            console.log('error:', e);
+            console.log('data:', data);
+            postApp(api)
+        });
     }
 
     function postApp(api) {
@@ -58,43 +57,49 @@ router.post('/', function(req, res) {
                 file: __outputPath + fileName
             }
         };
-       
+
 
         api.post('/apps', options, function(e, data) {
             if (e) throw e;
             console.log('data:', data);
             //once uploaded send the response back to UI
-            buildApp(api,data.id)
+            buildApp(api, data.id)
 
         });
     };
 
-    function buildApp(api,id){
+    function buildApp(api, id) {
 
         var options = {
-        form: {
-        data: {
-            platforms: [ 'android', 'winphone' ]
-        }
-    }
-};
+            form: {
+                data: {
+                    platforms: ['android', 'winphone']
+                }
+            }
+        };
 
-        api.post('/apps/'+id+'/build', options, function(e, data) {
+        api.post('/apps/' + id + '/build', options, function(e, data) {
             console.log('error:', e);
             console.log('data:', data);
-            downloadApp(api,data.id);
+            downloadApp(api, data.id);
         });
 
     };
 
-    function downloadApp(api,id) {
-    res.pipe(api.get('/apps/'+id+'/android').pipe(fs.createWriteStream('app.apk')));
+    function downloadApp(api, id) {
+        api.get('/apps/' + id + '/android').pipe(fs.createWriteStream(__outputPath + 'app.apk'));
+        api.get('/apps/' + id + '/winphone').pipe(fs.createWriteStream(__outputPath + 'app.xap'));
+        complete();
 
-  
-       
-   
+
 
     };
+
+    function complete(){
+         res.send({
+            "message": "finished"
+        });
+    }
 
     //rename the file 
     fs.renameSync(__outputPath + aliasFileName, __outputPath + fileName);
