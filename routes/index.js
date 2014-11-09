ae0dc1a7c472fb9c5907e8481e6fb1d55c7bb8ab
ramanathanMuthuraman <ramanathan.gksvv@gmail.com>
2014-11-09 22:40:11 +0530

    initial push

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Archive Extractor' });
+  res.render('index', { title: 'Write once run anywhere' });
 });
 
 module.exports = router;
